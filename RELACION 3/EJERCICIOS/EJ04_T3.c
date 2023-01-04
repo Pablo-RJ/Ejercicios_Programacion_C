077574d81d02e6c1bd2d902affd364d307f49f6c
Pablo Rodríguez Jiménez <pablorjh@gmail.com>
2023-01-04 14:48:11 +0100

    Actualización EJ04_T3

--- RELACION 3/EJERCICIOS/EJ04_T3.c
+++ RELACION 3/EJERCICIOS/EJ04_T3.c
@@ -1,36 +1,35 @@
 /* EJERCICIO 4 - TEMA 3 */
 
-/* Realizar un programa para que escriba todos los n´umeros primos entre 1 y N, siendo N un n´umero
-introducido por el usuario. */
+/* Realizar un programa para que escriba todos los numeros primos entre 1 y N, siendo N un numero introducido por el usuario. */
 
 #include <stdio.h>
 
-int primo(int numero){
+int Numero_Primo(int numero){
 
-   int n_divisores=0, i=1;
-
-   while(i<=numero){
-      if(numero%i>0)
-         n_divisores = n_divisores;
-      else
-         n_divisores++
-   }
-   i++
-
-   if(n_divisores==2)
-      return 1
-   else
-      return 0;
+   int i, j, divisores;
+   
+   for(i=1; i<=numero; i++){
+      divisores=0;
+      for(j=1; j<=i; j++){
+         if(i%j==0){ 
+            divisores++;
+         }
+      }
+      if(divisores<=2){
+         printf("El numero %d es primo\n", i);
+      }
+   } 
+   return (0);  
 }
 
 int main(){
 
-   int numero, contador;
+   int numero, i;
 
    printf("Introduzca un numero positivo cualquiera hasta el cual quieras saber los numeros primos contenidos: ");
    scanf("%d", &numero);
 
-
-
+   Numero_Primo(numero);
+   
    return(0);
 }
